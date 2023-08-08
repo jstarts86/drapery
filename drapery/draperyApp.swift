d8893443816e46354a25f8ac142883fbcf365cd5
John Song <jstarts86@gmail.com>
2023-08-08 21:13:26 +0900

    Initial Commit

---
+++ drapery/draperyApp.swift
@@ -0,0 +1,17 @@
+//
+//  draperyApp.swift
+//  drapery
+//
+//  Created by John on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct draperyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
